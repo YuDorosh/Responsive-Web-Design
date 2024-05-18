9bc1b3fec27bebea6f1bab48776aa3007d47fea5
Yurii Dorosh <yurii.dorosh25@gmail.com>
2024-05-18 12:44:19 -0600

    header_block

--- src/route/index.js
+++ src/route/index.js
@@ -30,6 +30,7 @@ router.get('/template-1', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('template-1', {
+    layout: 'template-1',
     page: {
       title: 'template 1 Page',
     },
